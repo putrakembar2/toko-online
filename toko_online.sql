41772b21f33170ca2703d6370470cb9d36952fc8
yuwandianto <yuwandianto@gmail.com>
2021-11-29 14:42:45 +0800

    menambah database

---
+++ toko_online.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 29, 2021 at 07:41 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.3.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `toko_online`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_barang`
+--
+
+CREATE TABLE `data_barang` (
+  `id` int(11) NOT NULL,
+  `nama_barang` varchar(150) NOT NULL,
+  `harga` varchar(20) NOT NULL,
+  `terjual` varchar(10) NOT NULL,
+  `kategori` varchar(100) NOT NULL,
+  `gambar` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `data_barang`
+--
+
+INSERT INTO `data_barang` (`id`, `nama_barang`, `harga`, `terjual`, `kategori`, `gambar`) VALUES
+(1, 'Tripod HP dan CamDic', '150.000', '3', 'Tripod', 'tripod.jpg'),
+(2, 'Tripod Pendek', '100.000', '7', 'Tripod', 'tripod2.jpg'),
+(3, 'Headset Besar', '250.000', '2', 'Sound', 'headset.jpg'),
+(4, 'Headset Sony', '150.000', '10', 'Sound', 'headset2.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_barang`
+--
+ALTER TABLE `data_barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_barang`
+--
+ALTER TABLE `data_barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
